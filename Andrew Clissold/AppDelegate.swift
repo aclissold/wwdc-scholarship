3468a36f229b6efc637aed59ad29b157843ca372
Andrew Clissold <andrewclissold@gmail.com>
2015-04-20 19:28:54 -0400

    Initial commit

---
+++ Andrew Clissold/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Andrew Clissold
+//
+//  Created by Andrew Clissold on 4/20/15.
+//  Copyright (c) 2015 Andrew Clissold. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
---
+++ Andrew Clissold/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  Andrew Clissold
+//
+//  Created by Andrew Clissold on 4/20/15.
+//  Copyright (c) 2015 Andrew Clissold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
