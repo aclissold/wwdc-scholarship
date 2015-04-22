5b88969dcf11f1e2565f346bcbf84b663ec17245
Andrew Clissold <andrewclissold@gmail.com>
2015-04-21 20:45:46 -0400

    Create initial main view UI

--- Andrew Clissold/MainViewController.swift
+++ Andrew Clissold/MainViewController.swift
@@ -8,6 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var projectsButton: UIButton!
+    @IBOutlet weak var interestsButton: UIButton!
+    @IBOutlet weak var skillsButton: UIButton!
+    @IBOutlet weak var backgroundButton: UIButton!
+
+    override func viewDidLayoutSubviews() {
+        rotateFrames()
+    }
+
+    func rotateFrames() {
+        var rotationTransform = CGAffineTransformIdentity
+
+        rotationTransform = CGAffineTransformRotate(rotationTransform, CGFloat(M_PI_2))
+        interestsButton.layer.transform = CATransform3DMakeAffineTransform(rotationTransform)
+
+        rotationTransform = CGAffineTransformRotate(rotationTransform, CGFloat(M_PI_2))
+        skillsButton.layer.transform = CATransform3DMakeAffineTransform(rotationTransform)
+
+        rotationTransform = CGAffineTransformRotate(rotationTransform, CGFloat(M_PI_2))
+        backgroundButton.layer.transform = CATransform3DMakeAffineTransform(rotationTransform)
+
+        // Scoot the left and right buttons closer to the edge of the screen to match the top and bottom buttons.
+        interestsButton.frame.origin.x -= (interestsButton.frame.size.height/2 - interestsButton.frame.size.width/4)
+        backgroundButton.frame.origin.x += (backgroundButton.frame.size.height/2 - backgroundButton.frame.size.width/4)
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
+    override func supportedInterfaceOrientations() -> Int {
+        return Int(UIInterfaceOrientationMask.Portrait.rawValue)
+    }
+
+    override func preferredInterfaceOrientationForPresentation() -> UIInterfaceOrientation {
+        return .Portrait
+    }
+
+    @IBAction func projectsButtonTapped(sender: UIButton) {
+
+    }
+
+    @IBAction func interestsButtonTapped(sender: UIButton) {
+
+    }
+
+    @IBAction func skillsButtonTapped(sender: UIButton) {
+
+    }
+
+    @IBAction func backgroundButtonTapped(sender: UIButton) {
+
+    }
 
 }
