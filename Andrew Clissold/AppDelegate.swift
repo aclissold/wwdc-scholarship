12edf992a75b3386f68265d90480a659573efd69
Andrew Clissold <andrewclissold@gmail.com>
2015-04-25 23:28:27 -0400

    Make the theme red a global constant

--- Andrew Clissold/AppDelegate.swift
+++ Andrew Clissold/AppDelegate.swift
@@ -19,22 +19,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func theme() {
-        let redColor = UIColor(red: 0xAA/0xFF, green: 0, blue: 0, alpha: 1)
         let lightGrayColor = UIColor(white: 0.85, alpha: 1)
         let toolbarBackgroundColor = UIColor(red: 0xF5/0xFF, green: 0xF5/0xFF, blue: 0xF5/0xFF, alpha: 0.67)
 
         let navigationBarAppearance = UINavigationBar.appearance()
-        navigationBarAppearance.tintColor = redColor
-        navigationBarAppearance.titleTextAttributes = [NSForegroundColorAttributeName: redColor,
+        navigationBarAppearance.tintColor = andrewClissoldRed
+        navigationBarAppearance.titleTextAttributes = [NSForegroundColorAttributeName: andrewClissoldRed,
             NSFontAttributeName: UIFont(name: "HelveticaNeue-Light", size: 17)!]
 
         let pageControlAppearance = UIPageControl.appearance()
-        pageControlAppearance.currentPageIndicatorTintColor = redColor
+        pageControlAppearance.currentPageIndicatorTintColor = andrewClissoldRed
         pageControlAppearance.pageIndicatorTintColor = lightGrayColor
         pageControlAppearance.backgroundColor = toolbarBackgroundColor
 
         let barButtonItemApperance = UIBarButtonItem.appearance()
-        barButtonItemApperance.tintColor = redColor
+        barButtonItemApperance.tintColor = andrewClissoldRed
     }
 
 }
---
+++ Andrew Clissold/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Andrew Clissold
+//
+//  Created by Andrew Clissold on 4/25/15.
+//  Copyright (c) 2015 Andrew Clissold. All rights reserved.
+//
+
+import UIKit
+
+let andrewClissoldRed = UIColor(red: 0xAA/0xFF, green: 0, blue: 0, alpha: 1)
