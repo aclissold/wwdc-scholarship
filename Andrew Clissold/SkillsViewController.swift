95cb1159b7f571d81004e53863ffa5bd8b5b99d2
Andrew Clissold <andrewclissold@gmail.com>
2015-04-26 17:02:19 -0400

    Skills: add a horizontally-scrollable label

--- Andrew Clissold/SkillsViewController.swift
+++ Andrew Clissold/SkillsViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import Foundation
 
 class SkillsViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         title = "Skills"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: "done:")
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        let textWidth = (label.text! as NSString).boundingRectWithSize(label.frame.size,
+            options: .UsesLineFragmentOrigin,
+            attributes: [NSFontAttributeName: label.font],
+            context: nil).size.width
+        scrollView.contentSize.width = textWidth
+        scrollView.contentInset = UIEdgeInsetsZero
     }
 
     func done(sender: UIBarButtonItem) {
