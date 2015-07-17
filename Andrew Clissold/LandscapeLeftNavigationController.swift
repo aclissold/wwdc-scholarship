1117b1a1dce2167396a4a3e25f19455355d30516
Andrew Clissold <andrewclissold@gmail.com>
2015-07-16 22:59:17 -0400

    Delete accidental newline

--- Andrew Clissold/LandscapeLeftNavigationController.swift
+++ Andrew Clissold/LandscapeLeftNavigationController.swift
@@ -17,6 +17,5 @@ class LandscapeLeftNavigationController: UINavigationController {
     override func preferredInterfaceOrientationForPresentation() -> UIInterfaceOrientation {
         return .LandscapeLeft
     }
-
    
 }
